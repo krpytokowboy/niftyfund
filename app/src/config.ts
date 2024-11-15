7d8ef1777257fe89996a61e6d128ee12328f18b3
CK <kryptokowboy@proton.me>
2024-11-15 13:30:53 +0700

    No fomo

--- app/src/config.ts
+++ app/src/config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
           //pagination: 'count',
           title: 'Invest with Peace of Mind and Autonomy',
           description:
-            "<b>Say NO to FOMO</b>... This is just the beginning... You are here early... Congratulations! 🥳 🫶 🎉",
+            "<b>Say NO to FOMO</b>!!! This is just the beginning... You are here early... Congratulations! 🥳 🫶 🎉",
           button: 'Next',
         },
 
